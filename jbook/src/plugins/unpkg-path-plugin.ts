d8ade338e7f6c685f9098839fcc4f3c39a407f49
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-09-10 18:02:00 -0700

    add caching code portion

--- jbook/src/plugins/unpkg-path-plugin.ts
+++ jbook/src/plugins/unpkg-path-plugin.ts
@@ -58,14 +58,24 @@ export const unpkgPathPlugin = () => {
 
         // Check to see if we have already fetched this file
         // and if it is in the cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path)
+
+        if (cachedResult) {
+          return cachedResult
+        }
 
         const { data, request } = await axios.get(args.path)
-        // store response in cache
-        return {
+
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname
         }
+
+        // store response in cache
+        await fileCache.setItem(args.path, result)
+
+        return result
       });
     },
   };
